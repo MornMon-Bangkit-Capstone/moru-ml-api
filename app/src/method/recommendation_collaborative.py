b2067da9875e3725f87a36e0793cddcd32de81b4
zer0-911 <mohiqbalf5@gmail.com>
2023-06-06 16:41:03 +0700

    feat: model revision 🚀

--- app/src/method/recommendation_collaborative.py
+++ app/src/method/recommendation_collaborative.py
@@ -8,6 +8,7 @@ def recommendation(user_id: str):
     loaded = tf.saved_model.load(path)
     id = int(user_id)
     scores, titles = loaded([id])
- 
+    cek = titles[0][:20]
+    print(cek)
     return titles.numpy().tolist()
     
